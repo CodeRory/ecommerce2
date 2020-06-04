c7c2721a9ddde91be89b1d581fb7a15a8daeeda3
Rory <coderory@gmail.com>
2020-06-04 16:58:04 +0200

    Header.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
+import SearchAppBar from './components/header/header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-          Learn React
-        
+        <SearchAppBar />
       </header>
     </div>
   );
