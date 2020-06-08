2522adadbdd39e3b466a8dd4918655f01fe1af35
Rory <coderory@gmail.com>
2020-06-08 14:12:27 +0200

    Cards and Cart.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import SearchAppBar from './components/header/header';
 import SecondNav from './components/secondNav/secondNav';
 import Slider from './components/slider/slider';
+
+import Navbar from './components/cards/Navbar';
+import ProductList from './components/cards/ProductList';
+import Details from './components/cards/Details';
+import Cart from './components/cards/cart/Cart';
+import Default from './components/cards/Default';
+import Modal from './components/cards/Modal';
+
+
 
 function App() {
   return (
@@ -18,6 +28,14 @@ function App() {
           <Slider />
         </div>
         <div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+        <Modal />
          
         </div>
       </div>
