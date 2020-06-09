317e7b09c8a6b67dd1a80da3feef04f4f00f9c21
Rory <coderory@gmail.com>
2020-06-09 16:34:16 +0200

    Editing description area.

--- src/components/cards/Details.js
+++ src/components/cards/Details.js
@@ -3,10 +3,12 @@ import { ProductConsumer } from "../../context";
 import { Link } from "react-router-dom";
 import { ButtonContainer } from "./Button";
 
+import '../cards/cards.css';
+
 export default class Details extends Component {
   render() {
     return (
-      <ProductConsumer>
+      <ProductConsumer >
         {value => {
           const {
             id,
@@ -18,7 +20,7 @@ export default class Details extends Component {
             inCart
           } = value.detailProduct;
           return (
-            <div className="container py-5">
+            <div className="container py-5" id='descriptionContainer'>
               {/* title */}
               <div className="row">
                 <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
@@ -33,23 +35,23 @@ export default class Details extends Component {
                 </div>
                 {/* product text */}
                 <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
-                  <h2>model: {title}</h2>
+                  <h2>Model: {title}</h2>
                   <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
-                    made by: <span className="text-uppercase">{company}</span>
+                    Made by: <span className="text-uppercase">{company}</span>
                   </h4>
                   <h4 className="text-blue">
                     <strong>
-                      price : <span>$</span> {price}
+                      Price : <span>$</span> {price}
                     </strong>
                   </h4>
                   <p className="text-capitalize font-weight-bold mt-3 mb-0">
-                    product description:
+                    Product description:
                   </p>
                   <p className="text-muted lead">{info}</p>
                   {/* buttons */}
                   <div>
                     <Link to="/">
-                      <ButtonContainer>back to products</ButtonContainer>
+                      <ButtonContainer>Back to offers</ButtonContainer>
                     </Link>
                     <ButtonContainer
                       cart
