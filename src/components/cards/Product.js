9bcafe5c8cca4d6946fde4d0ebdbbd42965bd9a1
Rory <coderory@gmail.com>
2020-06-09 16:20:24 +0200

    Changes on Cards.

--- src/components/cards/Product.js
+++ src/components/cards/Product.js
@@ -17,6 +17,7 @@ export default class Product extends Component {
         <div className="card">
           <ProductConsumer >
             {value => (
+              <div>
               <div 
                 className="img-container p-5"
                 onClick={() => value.handleDetail(id)}
@@ -41,16 +42,24 @@ export default class Product extends Component {
                   )}
                 </button>
               </div>
-            )}
-          </ProductConsumer>
-          {/* Card footer */}
-          <div className="card-footer d-flex justify-content-between">
-            <p className="align-self-center mb-0" >{title}</p>
-            <h5 className="text-blue font-italic mb-0">
+              <div 
+              className="img-container p-5"
+              onClick={() => value.handleDetail(id)}
+            >
+              <div className="card-footer d-flex justify-content-between" >
+            <Link className="align-self-center mb-0" to="/details" id='titleOfProduct'>{title}</Link>
+            <h5 className="text-blue font-italic mb-0" id='productPrice'>
               <span className="mr-1">$</span>
               {price}
             </h5>
           </div>
+          </div>
+          </div>
+
+              
+            )}
+          </ProductConsumer>
+          
         </div>
       </ProductWrapper>
     );
@@ -71,16 +80,18 @@ const ProductWrapper = styled.div`
   .card {
     border-color: transparent;
     transition: all 1s linear;
+    text-decoration: none;
   }
   .card-footer {
     background: transparent;
     border-top: transparent;
     transition: all 0.5s linear;
+    text-decoration: none;
   }
   &:hover {
     .card {
-      border: 0.04rem solid rgba(0, 0, 0, 0.2);
-      box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.2);
+      /* border: 0.04rem solid rgba(0, 0, 0, 0.2);
+      box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.2) */;
     }
     .card-footer {
       background: rgba(247, 247, 247);
