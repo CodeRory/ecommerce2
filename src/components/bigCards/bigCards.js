cad2b85e73b162fa5672ec23c9f96d0cda808034
Rory <coderory@gmail.com>
2020-06-09 13:16:58 +0200

    Changes.

--- src/components/bigCards/bigCards.js
+++ src/components/bigCards/bigCards.js
@@ -92,14 +92,14 @@ export default function BigCards() {
                 <a href='/#' className="firstBigPic">
                   <div className='textContainer'>
                     <h2 className="titleBigOne">Lorem Ipsum 1</h2>
-                    <p className="subBigOne">Aenean velit felis, interdum tristique odio eget, tempor convallis felis. Phasellus et dui purus. Donec vulputate orci sed purus venenatis, a finibus eros tristique. Phasellus faucibus, ligula ac porta rutrum, nisl enim commodo tortor, vitae euismod ex nisl id est.</p>
+                    <p className="subBigOne">Aenean velit felis, interdum tristique odio eget, tempor convallis felis. Phasellus et dui purus. Donec vulputate orci sed purus venenatis, a finibus eros tristique.</p>
                   </div>
                     </a>
                     
                 <a href='/#' className="secondBigPic">
                     <div className='textContainer'>
                     <h2 className="titleBigOne">Lorem Ipsum 2</h2>
-                    <p className="subBigOne">Proin odio neque, mattis eu augue eu, lacinia lacinia augue. Aliquam euismod nibh quis purus euismod bibendum. Proin eleifend ipsum at augue scelerisque, eget malesuada lacus varius. Proin pharetra quam id quam accumsan, eget volutpat arcu dapibus. </p>
+                    <p className="subBigOne">Proin odio neque, mattis eu augue eu, lacinia lacinia augue. Aliquam euismod nibh quis purus euismod bibendum. Proin eleifend ipsum at augue scelerisque, eget malesuada lacus varius. </p>
                     </div>
                 </a>
 
@@ -109,7 +109,7 @@ export default function BigCards() {
                 <div className="theBigPic">
                     <div className='textContainer'>
                     <h2 className="titleBigOne">Lorem Ipsum 3</h2>
-                    <p className="subBigOne">Pellentesque sit amet luctus sem, sit amet mollis libero. Fusce in maximus lacus. Etiam nulla mi, accumsan vel luctus eu, iaculis ut diam. Praesent mollis sollicitudin rhoncus. Ut dictum elit nec elit facilisis ornare. Donec vulputate orci sed purus venenatis, a finibus eros tristique. Phasellus faucibus, ligula ac porta rutrum, nisl enim commodo tortor, vitae euismod ex nisl id est.</p></div>
+                    <p className="subBigOne">Pellentesque sit amet luctus sem, sit amet mollis libero. Fusce in maximus lacus. Etiam nulla mi, accumsan vel luctus eu, iaculis ut diam. Praesent mollis sollicitudin rhoncus. Ut dictum elit nec elit facilisis ornare. Donec vulputate orci sed purus venenatis, a finibus eros tristique.</p></div>
                 </div>
             </a>
            
