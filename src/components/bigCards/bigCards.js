c52e03705f2b8ab5afe023d64c3c204b591f0977
Rory <coderory@gmail.com>
2020-06-09 13:12:33 +0200

    Responsiveness.

--- src/components/bigCards/bigCards.js
+++ src/components/bigCards/bigCards.js
@@ -52,7 +52,7 @@ const useStyles = makeStyles((theme) => ({
   
     return (
       <div>
-        <button onClick={handleClickOpen} className="btn" >
+        <button onClick={handleClickOpen} className="buttonBig" >
           {props.title}
         </button>
         <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
@@ -89,25 +89,29 @@ export default function BigCards() {
         <main className="sections">
             <div className='twoBigCards'>
                 
-                <one id="firstBigPic">
-                    <h2 className="title">Lorem Ipsum 1</h2>
-                    <p className="copy">Aenean velit felis, interdum tristique odio eget, tempor convallis felis. Phasellus et dui purus.</p><FullScreenDialog title='More Information' />
-                </one>
+                <a href='/#' className="firstBigPic">
+                  <div className='textContainer'>
+                    <h2 className="titleBigOne">Lorem Ipsum 1</h2>
+                    <p className="subBigOne">Aenean velit felis, interdum tristique odio eget, tempor convallis felis. Phasellus et dui purus. Donec vulputate orci sed purus venenatis, a finibus eros tristique. Phasellus faucibus, ligula ac porta rutrum, nisl enim commodo tortor, vitae euismod ex nisl id est.</p>
+                  </div>
+                    </a>
                     
-                <second className="secondBigPic">
-                    
-                    <h2 className="title">Lorem Ipsum 2</h2>
-                    <p className="copy">Proin odio neque, mattis eu augue eu, lacinia lacinia augue. Aliquam euismod nibh quis purus euismod bibendum. </p><FullScreenDialog title='Subscribe' />
-                </second>
+                <a href='/#' className="secondBigPic">
+                    <div className='textContainer'>
+                    <h2 className="titleBigOne">Lorem Ipsum 2</h2>
+                    <p className="subBigOne">Proin odio neque, mattis eu augue eu, lacinia lacinia augue. Aliquam euismod nibh quis purus euismod bibendum. Proin eleifend ipsum at augue scelerisque, eget malesuada lacus varius. Proin pharetra quam id quam accumsan, eget volutpat arcu dapibus. </p>
+                    </div>
+                </a>
 
             </div>   
             
-            <div className='oneBigCard'>                
-                <div className="content">
-                    <h2 className="title">Lorem Ipsum 3</h2>
-                    <p className="copy">Pellentesque sit amet luctus sem, sit amet mollis libero. Fusce in maximus lacus.</p><FullScreenDialog title='Reserve' /></div>
-                
-            </div>
+            <a href='/#' className='oneBigCard'>                
+                <div className="theBigPic">
+                    <div className='textContainer'>
+                    <h2 className="titleBigOne">Lorem Ipsum 3</h2>
+                    <p className="subBigOne">Pellentesque sit amet luctus sem, sit amet mollis libero. Fusce in maximus lacus. Etiam nulla mi, accumsan vel luctus eu, iaculis ut diam. Praesent mollis sollicitudin rhoncus. Ut dictum elit nec elit facilisis ornare. Donec vulputate orci sed purus venenatis, a finibus eros tristique. Phasellus faucibus, ligula ac porta rutrum, nisl enim commodo tortor, vitae euismod ex nisl id est.</p></div>
+                </div>
+            </a>
            
         </main>
 
